caa29a6d63e3b5d79e70873fe2fc3785016b5c8e
sten00777 <www.ep965@gmail.com>
2021-03-14 19:51:27 +0300

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let absolut = Math.abs(n);
+  let str = absolut.toString().split('').reverse().join('');
+  let res = parseInt(str);
+  return res;
 }
+
